16f4e24e225060ac9219eee6d2da0a509bca56df
Michael Hicks <hicks.michael04@gmail.com>
2022-03-06 15:24:26 -0700

    fix: remove root from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root '/'
   resources :merchant, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
