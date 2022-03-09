e8b6975e58d87a17d806c132e29057e3ef54b367
Michael Hicks <hicks.michael04@gmail.com>
2022-03-09 00:21:09 -0700

    fix: Holidays instance variable in index BD controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,7 +1,7 @@
 class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @holidays = BulkDiscountsFacade.holiday_info
+    @holidays = HolidayFacade.country_holidays("US") #not specified as params passed
   end
 
   def show
