1afe0db44f3c98a5f678a20e3531dc5bd62b9acd
Michael Hicks <hicks.michael04@gmail.com>
2022-03-08 23:23:38 -0700

    Refactor bulk_discounts and holiday facades

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -1,7 +1,7 @@
 class HolidayFacade
   class << self
     def country_holidays(country)
-      holidays = HolidayService.holidays(country)
+      holidays = HolidayService.holidays(country)[0..2]
       holidays.map do |holiday|
         Holiday.new(holiday)
       end
