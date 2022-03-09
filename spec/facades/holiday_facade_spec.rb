64438433809e6dbbe06716d49d7ec36b162b4846
Michael Hicks <hicks.michael04@gmail.com>
2022-03-09 00:23:41 -0700

    Create holiday facade spec and refactor BD index spec

--- spec/facades/holiday_facade_spec.rb
+++ spec/facades/holiday_facade_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe HolidayFacade do
   it "returns an array of holidays" do
     response = HolidayFacade.country_holidays("US")
-    binding.pry
-
+    expect(response).to be_a(Array)
+    expect(response.sample).to be_a(Holiday)
+    expect(response.count).to eq(3)
   end
 end
